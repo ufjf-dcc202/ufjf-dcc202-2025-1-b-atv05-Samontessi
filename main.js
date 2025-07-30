eb9f3f95414bf6c0f178543b836a4debf7d63105
Samuel Montessi do Amaral Silva <samuel.montessi@estudante.ufjf.br>
2025-07-30 17:20:58 -0300

    adiciona estado na tela

--- main.js
+++ main.js
@@ -1,26 +1,23 @@
+import { getTabuleiro } from "./discos.js";
+
 const eH1 = document.querySelector("h1");
 eH1.textContent = "Olá mundo!";
 
 const eTabuleiro = criaTabuleiro();
 document.body.append(eTabuleiro);
 
+const tabuleiro = getTabuleiro();
+
 for (let i = 0; i<7; i++){
     const eDisco = criaDisco();
     eTabuleiro.append(eDisco);
-    if(i<3){
-        eDisco.dataset.cor = "branco";
-    }
-    else if(i>3){
-        eDisco.dataset.cor = "preto";
-    }
-    else{
-        eDisco.dataset.cor = "transparente";
-    }
+    eDisco.dataset.cor = tabuleiro[i];
 }
 
-function criaDisco(){
+function criaDisco(cor){
     const novoDisco = document.createElement("div");
     novoDisco.classList.add("disco");
+    novoDisco.dataset.cor =  cor;
     return novoDisco;
 }
 
